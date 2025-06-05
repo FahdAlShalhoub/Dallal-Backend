b286a4b56dd13decf8cc16924c5b00f46de6611c
FahdAlShalhoub <fahdshalhoub>
2025-06-05 22:26:56 +0300

    FEAT: Added Get Listing Endpoint

--- Dallal-Backend-v2/Controllers/ListingsController.cs
+++ Dallal-Backend-v2/Controllers/ListingsController.cs
@@ -1,4 +1,6 @@
+using Dallal_Backend_v2.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dallal_Backend_v2.Controllers;
 
@@ -7,10 +9,25 @@ namespace Dallal_Backend_v2.Controllers;
 [Route("listings")]
 public class ListingsController : ControllerBase
 {
+    private readonly DatabaseContext _context;
+    public ListingsController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
     [HttpGet("recent")]
     public async Task<IActionResult> GetRecentListings()
     {
         return Ok();
+    }
+
+    [HttpGet(Name = "GetListings")]
+    [ProducesResponseType(typeof(IEnumerable<Listing>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Listings()
+    {
+        var listings = await _context.Listings.ToListAsync();
+        return Ok(listings);
     }
 
 }
